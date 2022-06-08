4b6c401f59c225b0091c4c8526f2bba50d52ea69
Stefan Klug <stefan.klug@cyberghost.ro>
2022-06-08 18:25:28 +0200

    Propagate errors when account details are null

--- profile/src/main/java/com/kape/profile/data/ProfileDatasourceImpl.kt
+++ profile/src/main/java/com/kape/profile/data/ProfileDatasourceImpl.kt
@@ -6,6 +6,7 @@ import com.kape.profile.models.Profile
 import com.kape.profile.models.Subscription
 import com.kape.profile.models.Subscription.Companion.DATE_FORMAT
 import com.privateinternetaccess.account.AccountAPI
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import org.koin.core.component.KoinComponent
@@ -17,12 +18,15 @@ class ProfileDatasourceImpl : ProfileDatasource, KoinComponent {
     private val api: AccountAPI by inject()
 
     override fun accountDetails(): Flow<Profile> = callbackFlow {
-        api.accountDetails { details, _ ->
+        api.accountDetails { details, errorList ->
             if (details != null) {
                 val subscription = Subscription(details.recurring, getExpirationDate(details.daysRemaining))
-                Profile(details.username, subscription)
+                trySend(Profile(details.username, subscription))
+            } else {
+                error(errorList)
             }
         }
+        awaitClose { channel.close() }
     }
 
     private fun getExpirationDate(daysRemaining: Int): String {
