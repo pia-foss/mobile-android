4390e0addaa251a70af7de038bda208e5258d604
Stefan Klug <stefan.klug@cyberghost.ro>
2022-06-13 17:29:29 +0200

    Profile module: Make the use case generic nullable (in case of exceptions)

--- profile/src/main/java/com/kape/profile/domain/GetProfileUseCase.kt
+++ profile/src/main/java/com/kape/profile/domain/GetProfileUseCase.kt
@@ -6,9 +6,13 @@ import kotlinx.coroutines.flow.flow
 
 class GetProfileUseCase(private val profile: ProfileDatasource) {
 
-    suspend fun getProfile(): Flow<Profile> = flow {
-        profile.accountDetails().collect { profile ->
-            emit(profile)
+    suspend fun getProfile(): Flow<Profile?> = flow {
+        try {
+            profile.accountDetails().collect { profile ->
+                emit(profile)
+            }
+        } catch (t: Throwable) {
+            emit(null)
         }
     }
 }
