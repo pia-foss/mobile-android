8b10e58bc2d7a7ad20b0d621e986f8654252f95d
Iva N <iva.nedeleva@kape.com>
2023-10-19 16:07:17 +0300

    PIA-811: Splash

--- features/splash/src/main/java/com/kape/splash/ui/SplashScreen.kt
+++ features/splash/src/main/java/com/kape/splash/ui/SplashScreen.kt
@@ -4,17 +4,23 @@ import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.kape.splash.ui.vm.SplashViewModel
 import com.kape.ui.elements.UiResources
 import com.kape.ui.theme.Space
+import com.kape.ui.utils.LocalColors
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -26,13 +32,18 @@ fun SplashScreen(viewModel: SplashViewModel = koinViewModel()) {
         },
     )
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LocalColors.current.background),
+    ) {
         Image(
-            painter = painterResource(id = UiResources.bigAppLogo),
-            contentDescription = "logo",
+            painter = painterResource(id = com.kape.ui.R.drawable.ic_pia_logo),
+            contentDescription = null,
             modifier = Modifier
                 .align(Alignment.Center)
-                .padding(Space.HUGE),
+                .width(200.dp)
+                .height(80.dp),
         )
     }
 
