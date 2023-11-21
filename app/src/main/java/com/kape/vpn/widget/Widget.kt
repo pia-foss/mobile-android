1c930a4a8e0478c08e6303fe3679422f095f1b20
Iva N <iva.nedeleva@kape.com>
2023-11-21 09:58:30 +0200

    PIA-964: Introduce GlanceTheme and PIA colors to widget

--- app/src/main/java/com/kape/vpn/widget/Widget.kt
+++ app/src/main/java/com/kape/vpn/widget/Widget.kt
@@ -1,18 +1,31 @@
 package com.kape.vpn.widget
 
 import android.content.Context
+import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Column
+import androidx.glance.layout.padding
 import androidx.glance.text.Text
 
 class Widget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         // TODO: to be implemented by https://polymoon.atlassian.net/browse/PIA-930
         provideContent {
-            Text(
-                text = "sample widget",
-            )
+            GlanceTheme(colors = WidgetColors.colors) {
+                Column(
+                    modifier = GlanceModifier.background(GlanceTheme.colors.background)
+                        .padding(16.dp),
+                ) {
+                    Text(
+                        text = "sample widget",
+                    )
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/kape/vpn/widget/WidgetColors.kt
@@ -0,0 +1,8 @@
+package com.kape.vpn.widget
+
+import androidx.glance.material3.ColorProviders
+import com.kape.ui.theme.LightColorScheme
+
+object WidgetColors {
+    val colors = ColorProviders(LightColorScheme)
+}
