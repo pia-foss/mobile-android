aee201ac62da65b1efd322d9ca986c82413065ca
Iva N <iva.nedeleva@kape.com>
2023-12-13 12:50:07 +0200

    PIA-158: About feature module skeleton

---
+++ features/about/build.gradle.kts
@@ -0,0 +1,59 @@
+import Dependencies.desugarJdkLibs
+import Dependencies.implementFeatureModule
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jlleitschuh.gradle.ktlint")
+    id("de.mannodermaus.android-junit5") version "1.8.2.0"
+}
+
+android {
+    namespace = "com.kape.about"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    flavorDimensions.add("provider")
+    productFlavors {
+        create("amazon") {
+            dimension = "provider"
+        }
+        create("google") {
+            dimension = "provider"
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    testOptions {
+        unitTests {
+            isReturnDefaultValues = true
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring(desugarJdkLibs)
+
+    implementation(project(":core:router"))
+    implementation(project(":core:utils"))
+    implementation(project(":capabilities:ui"))
+    implementation(project(":features:appbar"))
+
+    implementFeatureModule()
+}
