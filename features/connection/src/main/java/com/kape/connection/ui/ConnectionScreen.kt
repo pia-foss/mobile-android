90130e05f1829f27179bfe9ac7e8c7c69a0005c6
Iva N <iva.nedeleva@kape.com>
2023-12-18 12:29:47 +0200

    PIA-1052: Snooze refactored

--- features/connection/src/main/java/com/kape/connection/ui/ConnectionScreen.kt
+++ features/connection/src/main/java/com/kape/connection/ui/ConnectionScreen.kt
@@ -26,9 +26,7 @@ import androidx.compose.ui.unit.dp
 import com.kape.appbar.view.AppBar
 import com.kape.appbar.view.AppBarType
 import com.kape.appbar.viewmodel.AppBarViewModel
-import com.kape.connection.ui.tiles.Snooze
 import com.kape.connection.ui.vm.ConnectionViewModel
-import com.kape.connection.utils.SnoozeInterval
 import com.kape.portforwarding.data.model.PortForwardingStatus
 import com.kape.sidemenu.ui.SideMenu
 import com.kape.ui.elements.Screen
@@ -38,7 +36,9 @@ import com.kape.ui.tiles.IPTile
 import com.kape.ui.tiles.LocationPicker
 import com.kape.ui.tiles.QuickConnect
 import com.kape.ui.tiles.QuickSettings
+import com.kape.ui.tiles.Snooze
 import com.kape.ui.tiles.Traffic
+import com.kape.ui.utils.SnoozeInterval
 import com.kape.vpnconnect.utils.ConnectionManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
