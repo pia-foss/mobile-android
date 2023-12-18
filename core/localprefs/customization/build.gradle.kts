dcb319463d4573adb40c7eeca0657adb8206ca3e
Iva N <iva.nedeleva@kape.com>
2023-12-18 08:53:09 +0200

    PIA-1051: Customization prefs

--- core/localprefs/customization/build.gradle.kts
+++ core/localprefs/customization/build.gradle.kts
@@ -14,7 +14,15 @@ android {
     defaultConfig {
         minSdk = 24
     }
-
+    flavorDimensions.add("provider")
+    productFlavors {
+        create("amazon") {
+            dimension = "provider"
+        }
+        create("google") {
+            dimension = "provider"
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -27,5 +35,6 @@ android {
 
 dependencies {
     implementation(project(":core:utils"))
+    implementation(project(":core:localprefs:customization:data"))
     implementSerialization()
 }
