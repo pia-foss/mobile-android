41135e6e06fc36816bae101daee432d1dbb88f51
Iva N <iva.nedeleva@kape.com>
2024-01-24 23:32:11 +0200

    PIA-1244: Added cript to check for updates

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -184,4 +184,11 @@ object Dependencies {
         val isStable = stableKeyword || regex.matches(version)
         return isStable.not()
     }
+
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
 }
