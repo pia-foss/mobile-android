3cec0f573a0c02b9fe4c07482e888ff92adb27c9
Iva N <iva.nedeleva@kape.com>
2024-02-06 14:15:24 +0200

    PIA-1159: Resolve merge conflicts

--- features/connection/src/main/java/com/kape/connection/di/ConnectionModule.kt
+++ features/connection/src/main/java/com/kape/connection/di/ConnectionModule.kt
@@ -32,7 +32,7 @@ private val localConnectionModule = module {
             get(),
             get(),
             get(),
-            get()
+            get(),
             get(),
         )
     }
