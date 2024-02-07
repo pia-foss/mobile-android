0122f125b532c894f13b2e45bfdbf33d5c2bc091
Iva N <iva.nedeleva@kape.com>
2024-02-07 13:32:01 +0200

    PIA-1317: Version updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.kape.vpn"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 600
+        versionName = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
