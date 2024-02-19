e029f3a18bdbfda1413b4f07bd68973c55a2b64b
Iva N <iva.nedeleva@kape.com>
2024-02-19 11:14:36 +0200

    PIA-1358: Fixed customization screen

--- core/localprefs/customization/data/src/main/java/com/kape/customization/data/ScreenElement.kt
+++ core/localprefs/customization/data/src/main/java/com/kape/customization/data/ScreenElement.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ScreenElement(
     val element: Element,
+    val name: String,
     var isVisible: Boolean = true,
     var isLocked: Boolean = false,
 )
