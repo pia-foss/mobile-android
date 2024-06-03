8e919d212469070a0f892018813f9362993693ee
Juan D <juan.docal@kape.com>
2024-06-03 09:56:31 +0200

    PIA-XXXX: Bump version to 4.0.6 (669)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,10 +24,10 @@ plugins {
     id("org.jetbrains.kotlinx.kover")
 }
 
-val googleAppVersionCode = 668
+val googleAppVersionCode = 669
 val amazonAppVersionCode = googleAppVersionCode.plus(10000)
 val noInAppVersionCode = googleAppVersionCode.plus(10000)
-val appVersionName = "4.0.5"
+val appVersionName = "4.0.6"
 
 android {
     namespace = "com.kape.vpn"
