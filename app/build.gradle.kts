296a74c95e989661d0d2660e474584d3e42db7ec
Iva <103037123+kp-iva-nedeleva@users.noreply.github.com>
2024-06-05 17:25:06 +0300

    PIA-1904: Hotfix: Crash on missing field (#18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,10 +24,10 @@ plugins {
     id("org.jetbrains.kotlinx.kover")
 }
 
-val googleAppVersionCode = 669
+val googleAppVersionCode = 670
 val amazonAppVersionCode = googleAppVersionCode.plus(10000)
 val noInAppVersionCode = googleAppVersionCode.plus(10000)
-val appVersionName = "4.0.6"
+val appVersionName = "4.0.7"
 
 android {
     namespace = "com.kape.vpn"
