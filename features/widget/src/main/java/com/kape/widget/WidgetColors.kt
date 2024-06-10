1210506726fd6b06dd39f0642557980bdf1ba209
Iva N <iva.nedeleva@kape.com>
2024-06-10 09:23:36 +0300

    PIA-1922: Added widget dark mode support

--- features/widget/src/main/java/com/kape/widget/WidgetColors.kt
+++ features/widget/src/main/java/com/kape/widget/WidgetColors.kt
@@ -1,8 +1,9 @@
 package com.kape.widget
 
 import androidx.glance.material3.ColorProviders
+import com.kape.ui.theme.DarkColorScheme
 import com.kape.ui.theme.LightColorScheme
 
 object WidgetColors {
-    val colors = ColorProviders(LightColorScheme)
+    val colors = ColorProviders(light = LightColorScheme, dark = DarkColorScheme)
 }
