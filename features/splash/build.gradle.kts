a0bda172af96538beae0c522c1664c04af5361c3
Iva N <iva.nedeleva@kape.com>
2024-06-20 14:50:10 +0300

    PIA-1959: removed subscription references

--- features/splash/build.gradle.kts
+++ features/splash/build.gradle.kts
@@ -46,7 +46,6 @@ android {
 dependencies {
 
     implementation(project(":core:router"))
-    implementation(project(":core:payments"))
     implementation(project(":core:regions"))
     implementation(project(":capabilities:ui"))
     implementation(project(":capabilities:notifications"))
--- features/splash/src/main/java/com/kape/splash/ui/vm/SplashViewModel.kt
+++ features/splash/src/main/java/com/kape/splash/ui/vm/SplashViewModel.kt
@@ -1,16 +1,12 @@
 package com.kape.splash.ui.vm
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.kape.payments.domain.GetSubscriptionsUseCase
 import com.kape.router.ExitFlow
 import com.kape.router.Router
 import com.kape.vpnregions.utils.RegionListProvider
-import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 
 class SplashViewModel(
-    private val useCase: GetSubscriptionsUseCase,
     private val router: Router,
     private val regionListProvider: RegionListProvider,
 ) : ViewModel(), KoinComponent {
@@ -19,10 +15,6 @@ class SplashViewModel(
         if (regionListProvider.isDefaultList()) {
             regionListProvider.loadVpnServerLatencies()
         }
-        viewModelScope.launch {
-            useCase.getVpnSubscriptions().collect {
-                router.handleFlow(ExitFlow.Splash)
-            }
-        }
+        router.handleFlow(ExitFlow.Splash)
     }
 }
