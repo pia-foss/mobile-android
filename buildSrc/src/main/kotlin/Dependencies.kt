9ad694956336cac53fd340aea15b1d08b679adf4
Juan Docal <109512072+kp-juan-docal@users.noreply.github.com>
2024-06-20 13:30:53 +0200

    PIA-1960: Fix OVPN DIP not connecting (#42)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -161,7 +161,7 @@ object Dependencies {
     }
 
     fun DependencyHandler.implementVpnManager() {
-        add(IMPLEMENTATION, "com.kape.android:vpnmanager:0.3.7")
+        add(IMPLEMENTATION, "com.kape.android:vpnmanager:0.3.7-pia")
     }
 
     fun DependencyHandler.implementDrawablePainter() {
