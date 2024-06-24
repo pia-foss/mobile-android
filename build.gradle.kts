03db6a302832310d7b812b61df95b37eb9814483
Iva N <iva.nedeleva@kape.com>
2024-06-24 08:12:13 +0300

    PIA-1967: Gradle to 8.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("com.android.library") version "8.4.2" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1" apply false
