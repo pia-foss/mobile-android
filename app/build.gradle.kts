151ea781036e280fcbfecfcb5716c7881fc802c1
Iva <103037123+kp-iva-nedeleva@users.noreply.github.com>
2024-09-05 10:52:37 +0300

    Version update (#76)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,10 +24,10 @@ plugins {
     id("org.jetbrains.kotlinx.kover")
 }
 
-val googleAppVersionCode = 679
+val googleAppVersionCode = 680
 val amazonAppVersionCode = googleAppVersionCode.plus(10000)
 val noInAppVersionCode = googleAppVersionCode.plus(10000)
-val appVersionName = "4.0.16"
+val appVersionName = "4.0.17"
 
 android {
     namespace = "com.kape.vpn"
