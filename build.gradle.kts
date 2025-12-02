86f24d5fcc619e8e8fa8066b3ba5a57c6b75ab3b
Iva Nedeleva <iva.nedeleva@kape.com>
2025-12-02 17:56:31 +0200

    Versions plugin added back

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.application) apply false
@@ -7,6 +9,20 @@ plugins {
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.jvm) apply false
     alias(libs.plugins.ktlint) apply false
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+    // Enables checking all versions in libs.versions.toml
+    checkConstraints = true
+    checkBuildEnvironmentConstraints = true
+
+    // Optional: check Gradle version too
+    checkForGradleUpdate = true
+
+    // Optional: only show stable releases
+    rejectVersionIf { isNonStable(candidate.version) }
+    revision = "release"
 }
 
 subprojects {
@@ -17,3 +33,11 @@ subprojects {
         apply(plugin = "configuration")
     }
 }
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
