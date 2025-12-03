3995e401137f4c03d93b72000ad9d3ff6a89f0e0
Iva Nedeleva <iva.nedeleva@kape.com>
2025-12-03 11:35:02 +0200

    KM-13173: Dependency update

--- features/permissions/src/main/java/com/kape/permissions/di/PermissionsModule.kt
+++ features/permissions/src/main/java/com/kape/permissions/di/PermissionsModule.kt
@@ -4,8 +4,8 @@ import com.kape.permissions.data.VpnPermissionDataSourceImpl
 import com.kape.permissions.domain.IsVpnProfileInstalledUseCase
 import com.kape.permissions.domain.VpnPermissionDataSource
 import com.kape.permissions.ui.vm.PermissionsViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 fun permissionsModule(appModule: Module) = module {
@@ -15,7 +15,5 @@ fun permissionsModule(appModule: Module) = module {
 val localPermissionModule = module {
     single<VpnPermissionDataSource> { VpnPermissionDataSourceImpl(context = get()) }
     single { IsVpnProfileInstalledUseCase(dataSource = get()) }
-    viewModel {
-        PermissionsViewModel(get(), get(), get())
-    }
+    viewModelOf(::PermissionsViewModel)
 }
