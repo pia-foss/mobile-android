950512c7cd8836c579b08fa5e01e40e70e61da40
Iva Nedeleva <iva.nedeleva@kape.com>
2026-03-06 09:53:38 +0200

    TvWelcome updates

--- features/tvwelcome/src/main/java/com/kape/tvwelcome/ui/TvWelcomeScreen.kt
+++ features/tvwelcome/src/main/java/com/kape/tvwelcome/ui/TvWelcomeScreen.kt
@@ -1,6 +1,5 @@
 package com.kape.tvwelcome.ui
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -21,7 +21,8 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.kape.login.ui.vm.LoginViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.kape.router.LocalNavigator
 import com.kape.tvwelcome.ui.vm.TvWelcomeViewModel
 import com.kape.ui.R
 import com.kape.ui.mobile.elements.Screen
@@ -33,16 +34,17 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun TvWelcomeScreen() = Screen {
     val welcomeViewModel: TvWelcomeViewModel = koinViewModel()
-    val loginViewModel: LoginViewModel = koinViewModel()
+    val destination by welcomeViewModel.router.getNavigationState().collectAsStateWithLifecycle()
+    val navigator = LocalNavigator.current
     val initialFocusRequester = FocusRequester()
 
     LaunchedEffect(key1 = Unit) {
-        loginViewModel.checkUserLoggedIn()
         initialFocusRequester.requestFocus()
     }
 
-    BackHandler {
-        welcomeViewModel.exitApp()
+    destination?.let {
+        navigator.navigateTo(it)
+        welcomeViewModel.router.resetNavigation()
     }
 
     Row(
