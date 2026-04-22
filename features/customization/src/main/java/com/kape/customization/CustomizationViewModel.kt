d05f8c86b23941ed295d2567b1f2f066ba26a169
Iva Nedeleva <iva.nedeleva@kape.com>
2026-04-22 10:21:54 +0200

    KM-15639: Fixed elements displayed

--- features/customization/src/main/java/com/kape/customization/CustomizationViewModel.kt
+++ features/customization/src/main/java/com/kape/customization/CustomizationViewModel.kt
@@ -2,34 +2,50 @@ package com.kape.customization
 
 import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.kape.contracts.Router
 import com.kape.customization.data.Element
+import com.kape.data.ShadowsocksRegionSelection
 import com.kape.localprefs.data.customization.ScreenElement
 import com.kape.localprefs.prefs.CustomizationPrefs
+import com.kape.localprefs.prefs.SettingsPrefs
 import org.koin.core.annotation.KoinViewModel
 import org.koin.core.component.KoinComponent
 
 @KoinViewModel
 class CustomizationViewModel(
     private val prefs: CustomizationPrefs,
+    private val settingsPrefs: SettingsPrefs,
     val router: Router,
 ) : ViewModel(),
     KoinComponent {
-    private var items by mutableStateOf(prefs.getOrderedElements())
+    private val items = mutableStateListOf<ScreenElement>()
 
-    fun getOrderedElements(): List<ScreenElement> = items
+    fun getOrderedElements(): List<ScreenElement> {
+        val current = prefs.getElements()
+        current.forEach {
+            if (it.element == Element.ShadowsocksRegionSelection) {
+                if (settingsPrefs.isShadowsocksObfuscationEnabled()) {
+                    val visibleElement = it.apply { it.shouldDisplayElement = true }
+                    items.add(visibleElement)
+                } else {
+                    val invisibleElement = it.apply { it.shouldDisplayElement = false }
+                    items.add(invisibleElement)
+                }
+            } else {
+                items.add(it)
+            }
+        }
+        return items
+    }
 
     fun onMove(
         from: LazyListItemInfo,
         to: LazyListItemInfo,
     ) {
-        items =
-            items.toMutableList().apply {
-                add(to.index, removeAt(from.index))
-            }
+        items.add(to.index, items.removeAt(from.index))
     }
 
     fun saveOrder() = prefs.setOrderedElements(items)
@@ -41,6 +57,6 @@ class CustomizationViewModel(
         items.first { it.element == element }.apply {
             this.isVisible = isVisible
         }
-        prefs.setOrderedElements(items)
+        prefs.setElements(items)
     }
 }
--- features/customization/src/main/java/com/kape/customization/di/CustomizationModule.kt
+++ features/customization/src/main/java/com/kape/customization/di/CustomizationModule.kt
@@ -3,6 +3,7 @@ package com.kape.customization.di
 import com.kape.contracts.Router
 import com.kape.customization.CustomizationViewModel
 import com.kape.localprefs.prefs.CustomizationPrefs
+import com.kape.localprefs.prefs.SettingsPrefs
 import org.koin.core.annotation.KoinViewModel
 import org.koin.core.annotation.Module
 
@@ -11,6 +12,7 @@ class CustomizationModule {
     @KoinViewModel
     fun provideCustomizationViewModel(
         prefs: CustomizationPrefs,
+        settingsPrefs: SettingsPrefs,
         router: Router,
-    ): CustomizationViewModel = CustomizationViewModel(prefs, router)
+    ): CustomizationViewModel = CustomizationViewModel(prefs, settingsPrefs, router)
 }
