3256802c45c497c5cd61684fa68a4a29758b6e76
Iva Nedeleva <iva.nedeleva@kape.com>
2026-06-25 16:29:07 +0300

    KM-16957: Fixed Google Play snooze crash

--- capabilities/snooze/src/main/java/com/kape/snooze/SnoozeHandler.kt
+++ capabilities/snooze/src/main/java/com/kape/snooze/SnoozeHandler.kt
@@ -1,6 +1,5 @@
 package com.kape.snooze
 
-import android.os.CountDownTimer
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.work.ExistingWorkPolicy
@@ -11,6 +10,8 @@ import com.kape.data.WorkerTags
 import com.kape.localprefs.prefs.ConnectionPrefs
 import com.kape.vpnlauncher.VpnLauncher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import org.koin.core.annotation.Named
@@ -27,7 +28,7 @@ class SnoozeHandler(
 ) {
     val isSnoozeActive = mutableStateOf(false)
     val timeUntilResume = mutableIntStateOf(0)
-    private var countDownTimer: CountDownTimer? = null
+    private var countDownJob: Job? = null
 
     fun setSnooze(interval: Int) {
         ioScope.launch {
@@ -54,26 +55,20 @@ class SnoozeHandler(
             isSnoozeActive.value = false
             workManager.cancelUniqueWork(WorkerTags.SNOOZE_WORKER)
             connectionPrefs.setLastSnoozeEndTime(0)
-            countDownTimer?.cancel()
+            countDownJob?.cancel()
+            countDownJob = null
         }
     }
 
     private fun setCountdownTimer(end: Long) {
-        if (countDownTimer == null) {
-            countDownTimer =
-                object : CountDownTimer(end, MINUTE) {
-                    override fun onTick(millisUntilFinished: Long) {
-                        ioScope.launch {
-                            timeUntilResume.intValue =
-                                (((connectionPrefs.lastSnoozeEndTime.first() - Calendar.getInstance().timeInMillis) / MINUTE) + 1).toInt()
-                        }
-                    }
-
-                    override fun onFinish() {
-                        // no-op
-                    }
+        countDownJob?.cancel()
+        countDownJob =
+            ioScope.launch {
+                while (true) {
+                    timeUntilResume.intValue =
+                        (((connectionPrefs.lastSnoozeEndTime.first() - Calendar.getInstance().timeInMillis) / MINUTE) + 1).toInt()
+                    delay(MINUTE)
                 }
-        }
-        (countDownTimer as CountDownTimer).start()
+            }
     }
 }
