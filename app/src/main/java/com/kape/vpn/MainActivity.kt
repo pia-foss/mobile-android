4a3c3f24469079d1d9a0bfad36c1f1fdd422df77
Iva Nedeleva <iva.nedeleva@kape.com>
2026-07-28 14:08:23 +0200

    KM-17462: Enable screenshots on debug builds

--- app/src/main/java/com/kape/vpn/MainActivity.kt
+++ app/src/main/java/com/kape/vpn/MainActivity.kt
@@ -142,10 +142,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE,
-            WindowManager.LayoutParams.FLAG_SECURE,
-        )
+        if (!BuildConfig.DEBUG) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE,
+            )
+        }
         defineScreenOrientation()
         deepLinkLogin(intent)
         intent.action?.let {
